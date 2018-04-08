0462fb774c774670bf27649316d4615e434ad80e
Renata Kilian <renatakiliandev@gmail.com>
2018-04-07 21:11:30 -0300

    Adicionando teste de sorteio de duplas

---
+++ python/sorteio_duplas.py
@@ -0,0 +1,6 @@
+import random
+
+duplas = random.sample(range(1, 7), 6)
+print("Dupla 1: ", duplas[0], "e" ,duplas[1])
+print("Dupla 2: ", duplas[2], "e" ,duplas[3])
+print("Dupla 3: ", duplas[4], "e" ,duplas[5])
